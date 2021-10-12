62cf137d99b0c8e6406166120bbabac664ca16f3
Nikita Babko <niki.babko@gmail.com>
2021-10-12 22:27:16 +0700

    Fix script.

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-// Поиск осуществляется по имени
-
 const divTable = document.querySelector('.div-table');
-const divTableCol = document.querySelectorAll('.div-table');
 const input = document.getElementById('myInput');
 const count = document.querySelector('.count');
 
 const createTemplate = (user) => {
   return `
     <div class="div-table-row">
-        <div class="test_2">
+        <div class="col-block">
           <div>ID</div>
           <div class="div-table-col">${user.id}</div>
         </div>
 
-        <div class="test_2">
+        <div class="col-block">
           <div>Name</div>
           <div class="div-table-col">${user.name}</div>
         </div>
 
-        <div class="test_2">
+        <div class="col-block">
           <div>Username</div>
           <div class="div-table-col">${user.username}</div>
         </div>
 
-        <div class="test_2">
+        <div class="col-block">
           <div>Email</div>
           <div class="div-table-col">${user.email}</div>
         </div>
 
-        <div class="test_2">
+        <div class="col-block">
           <div>Address</div>
           <div class="div-table-col">${user.address.street}</div>
         </div>
@@ -42,37 +39,40 @@ input.addEventListener('input', () => {
 });
 
 const getData = (arr) => {
-  let firstArr = arr.filter((el) => {
-    return el.id < 6;
-  });
-
-  let newArr = firstArr.filter((el) => {
-    return el.name.toLowerCase().includes(input.value.toLowerCase());
-  });
-
-  if (newArr.length > 0) {
-    count.innerHTML = newArr.length;
-    divTable.innerHTML = '';
-    arr.forEach((el) => {
-      divTable.innerHTML += createTemplate(el);
+  let newArr;
+  let firstArr;
+  if (arr) {
+    firstArr = arr.filter((el) => {
+      return el.id < 6;
     });
-  }
 
-  if (newArr.length < 6) {
-    console.log(newArr.length);
-    count.innerHTML = newArr.length;
-    divTable.innerHTML = '';
-    newArr.forEach((el) => {
-      divTable.innerHTML += createTemplate(el);
+    newArr = firstArr.filter((el) => {
+      return el.name.toLowerCase().includes(input.value.toLowerCase());
     });
-  }
 
-  if (newArr.length === 5) {
-    count.innerHTML = ``;
-  }
+    if (newArr.length > 0) {
+      count.innerHTML = newArr.length;
+      divTable.innerHTML = '';
+      arr.forEach((el) => {
+        divTable.innerHTML += createTemplate(el);
+      });
+    }
 
-  if (newArr.length === 0) {
-    count.innerHTML = `Ничего не найдено`;
+    if (newArr.length < 6) {
+      count.innerHTML = newArr.length;
+      divTable.innerHTML = '';
+      newArr.forEach((el) => {
+        divTable.innerHTML += createTemplate(el);
+      });
+    }
+
+    if (newArr.length === 5) {
+      count.innerHTML = ``;
+    }
+
+    if (newArr.length === 0) {
+      count.innerHTML = `Ничего не найдено`;
+    }
   }
 };
 
